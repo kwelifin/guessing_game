c16132778404a862f255bb96b9a3ac81e1c0f3a0
Kwelifin <Kwelifinwork@gmail.com>
2018-01-10 13:46:53 +0300

    Basic input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Угадайте число!");
+
+    println!("Пожалуйств, введите предположение");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Не удалось прочитать строку");
+
+    println!("Ваша попытка: {}", guess);
 }
