9e2c0af7821dfd012dd3a75069a9832619f4ec8a
Kwelifin <Kwelifinwork@gmail.com>
2018-01-10 15:15:53 +0300

    Compare input and generated numbers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -16,5 +17,13 @@ fn main() {
 
     io::stdin().read_line(&mut guess).expect("Не удалось прочитать строку");
 
+    let guess: u32 = guess.trim().parse().expect("Пожалуйста, введите число!");
+
     println!("Ваша попытка: {}", guess);
+
+    match guess.cmp(&secred_number) {
+        Ordering::Less => println!("Слишком маленькое!"),
+        Ordering::Greater => println!("Слишком большое!"),
+        Ordering::Equal => println!("Вы выиграли!")        
+    }
 }
